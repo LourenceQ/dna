7851d3622d06bafed7c099110699a496535b30e2
lou <lou@lou.com>
2021-01-24 09:12:00 -0300

    website

--- dna_pro/dna_app/views.py
+++ dna_pro/dna_app/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'dna_app/index.html',)
+
+def aconselhamento(request):
+    return render(request,'dna_app/aconselhamento.html')
+
+def agua_magnetizada_saude(request):
+    return render(request,'dna_app/agua_magnetizada_saude.html')
+
+def agua_magnetizada(request):
+    return render(request,'dna_app/agua_magnetizada.html')
+
+def arteterapia(request):
+    return render(request,'dna_app/arteterapia.html')
+
+def constelacao_familiar(request):
+    return render(request,'dna_app/constelacao_familiar.html')
+
+def galeria(request):
+    return render(request,'dna_app/galeria.html')
+
+def introducao_constelacao_familiar(request):
+    return render(request,'dna_app/introducao_constelacao_familiar.html')
+
+def loja(request):
+    return render(request,'dna_app/loja.html')
+
+def mandalas(request):
+    return render(request,'dna_app/mandalas.html')
+
+def meditacao_mentalizacao(request):
+    return render(request,'dna_app/meditacao_mentalizacao.html')
+
+def pnl(request):
+    return render(request,'dna_app/pnl.html')
+
+def quemsomos(request):
+    return render(request,'dna_app/quemsomos.html')
+
+def desenvolvimento_pessoal(request):
+    return render(request,'dna_app/desenvolvimento_pessoal.html')
